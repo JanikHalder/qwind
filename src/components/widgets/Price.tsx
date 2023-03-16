a405f5b177e33641c7c387d66e0f944631af0116
JanikHalder <janikhalder@gmail.com>
2023-03-16 15:37:56 +0100

    changes

--- src/components/widgets/Price.tsx
+++ src/components/widgets/Price.tsx
@@ -9,32 +9,21 @@ export default component$(() => {
     datenow: new Date(),
   });
 
-  function getDays$() {
-    let allDays = [];
+  const getDays$ = () => {
+    const allDays = [];
     for (let day = 1; day <= 31; day++) {
       allDays.push(day);
     }
     return allDays;
   }
 
-  function getYears$() {
-    let allYears = [];
-    let getTodayYear = dateState.datenow.getFullYear();
+  const getYears$ = () => {
+    const allYears = [];
+    const getTodayYear = dateState.datenow.getFullYear();
     for (let day = getTodayYear; day >= getTodayYear - 100; day--) {
       allYears.push(day);
     }
     return allYears;
-  }
-
-  function getDaysInMonth$(month: number, year: number) {
-    const numDays = new Date(year, month, 0).getDate();
-    const daysArray = [];
-
-    for (let day = 1; day <= numDays; day++) {
-      daysArray.push(day);
-    }
-
-    return daysArray;
   }
 
   const months = [
@@ -668,7 +657,7 @@ export default component$(() => {
                                   class="block appearance-none w-full py-2 px-3 pr-8 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                                 >
                                   <option value="">Monat</option>
-                                  {months.map((month, index) => (
+                                  {months.map((month) => (
                                     <option
                                       key={`month-${month}`}
                                       value={month}
@@ -700,7 +689,7 @@ export default component$(() => {
                                   class="block appearance-none w-full py-2 px-3 pr-8 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                                 >
                                   <option value="">Jahr</option>
-                                  {getYears$().map((year, index) => (
+                                  {getYears$().map((year) => (
                                     <option key={`year-${year}`} value={year}>
                                       {year.toString()}
                                     </option>
